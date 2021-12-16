23da90b7968738f8f01016c1c9f8a4c85448989e
NoKavanFX <theo.louedec@gmail.com>
2021-12-16 12:39:55 +0100

    création de la fonction insertUser

---
+++ models/UserModel.php
@@ -0,0 +1,10 @@
+<?php
+    namespace Models;
+
+    class UserModel extends Database
+    {
+        function insertUser($firstname, $lastname, $email, $password){
+            $sql = "INSERT INTO `users`(`firstname`, `lastname`, `email`, `password`) VALUES ('$firstname','$lastname', '$email','$password')";
+            $this->bdd->query($sql);
+        }
+    }
