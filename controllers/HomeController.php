2667581f0fac9614a2f6f9315fa3f8d9afc4ee14
Sixcha <paulbora_akcan@hotmail.com>
2021-12-17 10:30:49 +0100

    Test with var_dump

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -13,7 +13,8 @@
             'addStatus' =>      0,      
             'addImage' =>       'https://www.citroen.fr/content/dam/citroen/master/b2c/models/new-c4-e/visualizer/front-view/New%20E-C4%20and%20C4_0MP00NWP_Blanc%20Banquise_FR_1280_720.png',
         ];
-            $this->model->addArticle();
+        var_dump($_POST);
+            //$this->model->addArticle();
         }
        
     }
